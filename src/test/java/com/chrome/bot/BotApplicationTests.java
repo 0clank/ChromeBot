4f2622c6fa2c46fd37df94288e26373ffdadc0ea
Qu1oX <danny1996@gmx.net>
2019-11-11 15:51:15 +0100

    Project upload

---
+++ src/test/java/com/chrome/bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrome.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
