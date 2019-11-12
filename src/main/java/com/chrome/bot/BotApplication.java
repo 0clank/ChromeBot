b8d45b9e98f0465a23e406fef2e9e92314c6328e
Qu1oX <danny1996@gmx.net>
2019-11-12 13:56:03 +0100

    Added IBrowser interface to handle a single browser object

--- src/main/java/com/chrome/bot/BotApplication.java
+++ src/main/java/com/chrome/bot/BotApplication.java
@@ -3,6 +3,7 @@ package com.chrome.bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BotApplication {
 
---
+++ src/main/java/com/chrome/bot/implement/web/IBrowser.java
@@ -0,0 +1,18 @@
+package com.chrome.bot.implement.web;
+
+/**
+ * ChromeBot <p>
+ * IBrowser.java <p>
+ * Represents a browser running on the local machine. <p>
+ * Allows to read, write and interact with elements on a website. <p>
+ * Using the x-path, path or name of that element. <p>
+ *
+ * @author q1x
+ * @version 1.0
+ * @since 12 Nov 2019 13:34
+ */
+public interface IBrowser {
+
+
+
+}
