0c0ff4f92de20d48f46d7bb96ba959c7bf8a4fdf
Qu1oX <danny1996@gmx.net>
2019-11-12 13:58:22 +0100

    Added javadoc to BotApplication.java

--- src/main/java/com/chrome/bot/BotApplication.java
+++ src/main/java/com/chrome/bot/BotApplication.java
@@ -3,10 +3,22 @@ package com.chrome.bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * ChromeBot <p>
+ * BotApplication.java <p>
+ * Entry point of "ChromeBot"
+ *
+ * @author q1x
+ * @version 1.0
+ * @since 12 Nov 2019 13:12
+ */
 @SpringBootApplication
 public class BotApplication {
 
+    /**
+     * Entry point of "ChromeBot"
+     * @param args Argument given to "ChromeBot"
+     */
     public static void main (String[] args) {
         SpringApplication.run(BotApplication.class, args);
     }
